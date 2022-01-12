8464b9c1c291286c5d4dfccbf65394ca7e8024d4
abrunello <brunello@bludata.com>
2022-01-12 16:46:50 +0100

    simple validation with state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,42 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true)
+
+  const onNameChange = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const name = enteredName;
+
+    if(name.trim() === ''){
+      setEnteredNameIsValid(false)
+      return
+    }
+
+    setEnteredNameIsValid(true)
+    console.log(name);
+
+    setEnteredName('')
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={onFormSubmit}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          value={enteredName}
+          type="text"
+          id="name"
+          onChange={onNameChange}
+        />
+        {!enteredNameIsValid && <p style={{
+          color: 'red'
+        }}>Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
