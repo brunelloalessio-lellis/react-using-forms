1f75a53babd7bb172d35bb22f899c302f886deaf
abrunello <brunello@bludata.com>
2022-01-13 09:58:28 +0100

    check complete form validity combining all the input validation state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,31 +3,44 @@ import { useState } from "react";
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== "" && enteredNameTouched;
+  const enteredEmailIsValid = enteredEmail.includes("@") && enteredEmailTouched;
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
-  const formIsValid = enteredNameIsValid
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+  const formIsValid = enteredNameIsValid && enteredEmailIsValid;
 
   const onNameChange = (event) => {
     setEnteredName(event.target.value);
+  };
+  const onEmailChange = (event) => {
+    setEnteredEmail(event.target.value);
   };
 
   const onFormSubmit = (event) => {
     event.preventDefault();
     setEnteredNameTouched(true);
 
-    if(!formIsValid) return
+    if (!formIsValid) return;
 
     setEnteredName("");
     setEnteredNameTouched(false);
   };
 
-  const onInputBlur = (event) => {
+  const onInputNameBlur = (event) => {
     setEnteredNameTouched(true);
   };
-
+  const onInputEmailBlur = (event) => {
+    setEnteredEmailTouched(true);
+  };
 
   const nameInputClasses = !nameInputIsInvalid
+    ? "form-control"
+    : "form-control invalid";
+
+  const emailInputClasses = !emailInputIsInvalid
     ? "form-control"
     : "form-control invalid";
 
@@ -40,7 +53,7 @@ const SimpleInput = () => {
           type="text"
           id="name"
           onChange={onNameChange}
-          onBlur={onInputBlur}
+          onBlur={onInputNameBlur}
         />
         {nameInputIsInvalid && (
           <p
@@ -49,6 +62,25 @@ const SimpleInput = () => {
             }}
           >
             Name must not be empty
+          </p>
+        )}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your Email</label>
+        <input
+          value={enteredEmail}
+          type="text"
+          id="email"
+          onChange={onEmailChange}
+          onBlur={onInputEmailBlur}
+        />
+        {emailInputIsInvalid && (
+          <p
+            style={{
+              color: "red",
+            }}
+          >
+            Please enter a valid email
           </p>
         )}
       </div>
